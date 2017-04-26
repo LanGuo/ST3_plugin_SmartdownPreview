01890556deee158f99d0125b03b5a3f2254cc97c
languo <lan.guo14@gmail.com>
2017-04-25 23:40:11 -0700

    edited smartdownpreview command to save out an html file containing the smartdown content being edited to ~/tmp/

--- smartdownpreview.py
+++ smartdownpreview.py
@@ -6,23 +6,52 @@ import sublime
 import sublime_plugin
 import webbrowser
 import base64
+import os
+import pdb
 
 class SmartdownpreviewCommand(sublime_plugin.TextCommand):  #sublime_plugin.EventListener):
+	
+	# if outputMode is 'html', save out an html file; if outputMode is 'dataURI', open a browser for preview using smartdown.site
+	outputMode = 'html' #'dataURI'
+
+	# Template location is within the same package, this is hard coded!
+	html_template = os.path.join(os.path.expanduser("~"), 'Library', 'Application Support', 'Sublime Text 3', 'Packages', 'SmartDownPreview', 'smartdown_template.html') 
+	#this html template contains placeholder for title and content
 
 	#def on_post_save(self, view):
 	def run(self, edit):
 		# saved file name, fullpath to the file being edited and saved
-		self.savedFileName = self.view.file_name()
-		if self.savedFileName.endswith('.md') or self.savedFileName.endswith('.mmd'):
-			full_url = self.generate_url()
-
-			preview = self.openUrl(full_url)
-			if not preview:
-				self.log('Smart down preview cannot be opened')
+		self.currentFilePath = self.view.file_name()
+		if self.currentFilePath.endswith('.md') or self.currentFilePath.endswith('.mmd'):
+			#full_url = self.generate_url()
+			#preview = self.openUrl(full_url)
+			currentFileName = os.path.basename(self.currentFilePath)
+			content = open(self.currentFilePath, "r").read()
+			self.previewFilePath = os.path.join(os.path.expanduser("~"), 'tmp')
+			if not os.path.exists(self.previewFilePath):
+				os.mkdir(self.previewFilePath)
+			previewFileFullPath = os.path.join(self.previewFilePath, "{}.html".format(currentFileName))
+			html_string = self.generate_html(title=currentFileName, content=content)
+			self.save_tmp_file(html_string, outFilePath=previewFileFullPath)
 
 		else:
 			self.log('This file is not SmartDown format!')
 			pass
+
+	def generate_html(self, title, content):
+		'''
+		Generates an html file from template and current file content, saves it locally. 
+		'''	
+		html_template = open(self.html_template, "r").read()
+		#pdb.set_trace()
+		# Replace title and content in template with actual file we're editing 
+		preview_html = html_template.replace('$title', title)
+		preview_html = preview_html.replace('$content', content)
+		return preview_html
+
+	def save_tmp_file(self, string, outFilePath):
+		with open(outFilePath, 'w') as outFile:
+			outFile.write(string)
 
 
 	def generate_url(self):
@@ -34,7 +63,7 @@ class SmartdownpreviewCommand(sublime_plugin.TextCommand):  #sublime_plugin.Even
 		return url
 
 	def convert_current_file_to_uri(self):
-		current_file = self.savedFileName
+		current_file = self.currentFilePath
 		data_uri = base64.b64encode(open(current_file, "rb").read())
 		#str = open(current_file, "rb").read()
 		#data64 = u''.join(base64.encodestring(str).splitlines())
